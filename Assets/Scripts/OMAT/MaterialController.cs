2f7f846f60690b379d9e0a5c042cd74387c0ff96
masaprot <128129552+masaprot@users.noreply.github.com>
2023-11-16 13:17:20 +0200

    WristMenu
    sekä materiaalien aloitus

---
+++ Assets/Scripts/OMAT/MaterialController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialController : MonoBehaviour
+{
+    public MeshRenderer controlledMesh;
+
+    public List<Material> meshMaterials;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
