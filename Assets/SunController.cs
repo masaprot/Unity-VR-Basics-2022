406c77ce5ca876bdcd748d7b3c4ca30e867f5d6a
masaprot <128129552+masaprot@users.noreply.github.com>
2023-11-27 16:45:49 +0200

    PC sun cotrolleriin quaternion

--- Assets/SunController.cs
+++ Assets/SunController.cs
@@ -1,11 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class SunController : MonoBehaviour
 {
+    public Light sun;
+    public Vector3 dayEuler, nightEuler;
+    Quaternion dayRotation, nightRotation;
+    public float duration = 1;
+    float timer;
+    bool fadeUp;
 
-    public bool isDay;
+    private void Awake()
+    {
+        dayRotation = Quaternion.Euler(dayEuler);
+        nightRotation = Quaternion.Euler(nightEuler);
+    }
+
+
+    private void Update()
+    {
+        if (fadeUp && timer < 1f)
+        {
+            timer += Time.deltaTime / duration;
+            if (timer > 1) timer = 1; // is we're ar or between 0 and 1
+            sun.transform.rotation = Quaternion.Slerp(nightRotation, dayRotation, timer);
+        }
+        else if (fadeUp == false && timer > 0f)
+        {
+            timer -= Time.deltaTime / duration;
+            if (timer < 0) timer = 0; // is we're ar or between 0 and 1
+            sun.transform.rotation = Quaternion.Slerp(nightRotation, dayRotation, timer); 
+        }    
+    }
+
+    public void ToggleDayNight()
+    {
+        if (fadeUp == true)
+            fadeUp = false;
+        else
+            fadeUp = true;
+    }
+
+
+
+
+
+
+    /*public bool isDay;
 
 
     void Update()
@@ -41,6 +84,6 @@ public class SunController : MonoBehaviour
         Vector3 rotationToAdd = new Vector3(-10, 0, 0);
         transform.Rotate(rotationToAdd);
     }
+    */
 
-   
 }
